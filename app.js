e6100722fa4c6659ede4f9b9af6dac031096308d
RYAN_TAMA <UNIQUELYTAMA9@gmail.com>
2022-12-19 11:19:54 +0100

    work done

--- app.js
+++ app.js
@@ -12,19 +12,30 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
-const { hashPassword } = require("./auth");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+// Protected Routes
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/movies/:id", movieHandlers.putMovies);
 app.put("/api/users/:id", userHandlers.putUsers);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-app.delete("/api/movies/:id", userHandlers.deleteUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
+app.use(verifyToken);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -56,7 +56,25 @@ const deleteUser = (req, res) => {
       res.status(500).send("Error deleting the user");
     });
 };
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
 
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
 const getUsers = (req, res) => {
   let sql = "select * from users";
   const sqlValues = [];
@@ -116,4 +134,5 @@ module.exports = {
   postUser,
   putUsers,
   deleteUser,
+  getUserByEmailWithPasswordAndPassToNext,
 };
